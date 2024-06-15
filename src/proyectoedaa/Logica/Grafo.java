9f090173ee5da30555ff03df6af64295771dfd7a
JosephFRodriguezB <rodriguezjoseph060906@gmail.com>
2024-06-15 15:00:35 -0500

    Ajuste de Nodos del Grafo

--- src/proyectoedaa/Logica/Grafo.java
+++ src/proyectoedaa/Logica/Grafo.java
@@ -1,90 +1,93 @@
 //11/04/24
-package proyectoedaa;
+package proyectoedaa.Logica;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
 
-public class Grafo_Listas_Adyacencia {
+public class Grafo {
     private int V;
-    private int A; 
-    private LinkedList<Arista>[] adj;
+    private int A;
+    private LinkedList<LinkedList<Arista>> adj;
+    private LinkedList<Vertice> nod;
 
     public int getV() {
         return V;
     }
 
-    public LinkedList<Arista>[] getAdj() {
+    public LinkedList<LinkedList<Arista>> getAdj() {
         return adj;
     }
-    
-    
-    public Grafo_Listas_Adyacencia(int nodos) {
+
+    public Grafo(int nodos) {
         this.V = nodos;
         this.A = 0;
-        this.adj = new LinkedList[nodos];
-        for(int i=0; i<V; i++){
-            adj[i] = new LinkedList<>();
-        }
+        this.adj = new LinkedList<LinkedList<Arista>>();
+        this.nod = new LinkedList<Vertice>();
     }
-    
-    public void imprimirGrafo(){
-        for(int i=0; i<V; i++){
-            System.out.print("\nVertice "+i+": ");
-            for(int j=0; j<adj[i].size(); j++){
-                System.out.print(adj[i].get(j).d+" ");
+
+    public void imprimirGrafo() {
+        for (int i = 0; i < V; i++) {
+            System.out.print("\nVertice " + i + ": ");
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                System.out.print(adj.get(i).get(j).d + " ");
             }
         }
         System.out.println("");
     }
-    public class Arista{
+
+    public class Arista {
         public int o, d, peso;
+
         public Arista(int o, int d, int peso) {
             this.o = o;
             this.d = d;
             this.peso = peso;
         }
     }
-    
-    public void agregarArista(int u, int v, int peso){
-        adj[u].add(new Arista(u, v, peso));
-        adj[v].add(new Arista(v, u, peso));
-        A++;
-    }
-    
-    public void bfs(int s){
-        boolean[] visited = new boolean[V];
-        Queue<Integer> q = new LinkedList<>();
-        visited[s] = true;
-        q.offer(s);
-        
-        while(!q.isEmpty()){
-            int u = q.poll();
-            System.out.print(u+" ");
-            for(Arista i: adj[u]){
-                if(!visited[i.d]){
-                    visited[i.d] = true;
-                    q.offer(i.d);
-                }
-            }
+
+    public class Vertice {
+        long Numero;
+        String Velocidad_adquirida;
+        String Direccion;
+        String Operadora;
+
+        public Vertice(long Numero, String Velocidad_adquirida, String Direccion, String Operadora){
+            this.Numero = Numero;
+            this.Velocidad_adquirida = Velocidad_adquirida;
+            this.Direccion = Direccion;
+            this.Operadora = Operadora;
         }
     }
-    public void dfs(int s){
+
+    public void agregarVertice(long Numero, String Velocidad_adquirida, String Direccion, String Operadora, int u, int v, int peso){
+        nod.add(new Vertice(Numero, Velocidad_adquirida, Direccion, Operadora));
+        adj.add(new LinkedList<Arista>());
+        agregarArista(u, v, peso);
+    }
+    public void agregarArista(int u, int v, int peso) {
+        LinkedList<Arista> aux = new LinkedList<>();
+        aux =adj.get(u);
+        aux.add(new Arista(u, v, peso));
+        adj.set(u, aux);
+        aux = adj.get(v);
+        aux.add(new Arista(v, u, peso));
+        adj.set(v, aux);
+        A++;
+    }
+
+    public void dfs(int s) {
         boolean[] visited = new boolean[V];
         Stack<Integer> stack = new Stack<>();
         stack.push(s);
-        
+
         while (!stack.isEmpty()) {
             int u = stack.pop();
             if (!visited[u]) {
                 visited[u] = true;
                 System.out.print(u + " ");
-                for (Arista i : adj[u]) {
+                for (Arista i : adj.get(u)) {
                     if (!visited[i.d]) {
                         stack.push(i.d);
                     }
@@ -92,7 +95,7 @@ public class Grafo_Listas_Adyacencia {
             }
         }
     }
-    
+
     /////// ARBOL RECUBRIDOR MINIMO ///////
     public boolean hayCiclo(int origen, int destino) {
         boolean[] visited = new boolean[V];
@@ -103,7 +106,7 @@ public class Grafo_Listas_Adyacencia {
             int u = stack.pop();
             if (!visited[u]) {
                 visited[u] = true;
-                for (Arista i : adj[u]) {
+                for (Arista i : adj.get(u)) {
                     if (i.d == destino) {
                         return true;
                     }
@@ -115,30 +118,30 @@ public class Grafo_Listas_Adyacencia {
         }
         return false;
     }
-    
-    public Grafo_Listas_Adyacencia Kruskal() {
-        Grafo_Listas_Adyacencia Arbol_Recubridor_Minimo = new Grafo_Listas_Adyacencia(V);
+
+    public Grafo Kruskal() {
+        Grafo Arbol_Recubridor_Minimo = new Grafo(V);
         LinkedList<Arista> AristasOrdenadas = new LinkedList<>();
-        for(LinkedList<Arista> i : adj){
-            for(Arista j : i){
+        for (LinkedList<Arista> i : adj) {
+            for (Arista j : i) {
                 AristasOrdenadas.add(j);
             }
         }
-        
+
         Collections.sort(AristasOrdenadas, (a1, a2) -> Integer.compare(a1.peso, a2.peso));
-        for(Arista i : AristasOrdenadas){
-            if(!Arbol_Recubridor_Minimo.hayCiclo(i.o, i.d)){
+        for (Arista i : AristasOrdenadas) {
+            if (!Arbol_Recubridor_Minimo.hayCiclo(i.o, i.d)) {
                 Arbol_Recubridor_Minimo.agregarArista(i.o, i.d, i.peso);
             }
         }
 
         return Arbol_Recubridor_Minimo;
     }
-    
-    public Grafo_Listas_Adyacencia Prims() {
+
+    public Grafo Prims() {
         boolean[] visited = new boolean[V];
         Stack<Integer> stack = new Stack<>();
-        Grafo_Listas_Adyacencia Arbol_Recubridor_Minimo = new Grafo_Listas_Adyacencia(V);
+        Grafo Arbol_Recubridor_Minimo = new Grafo(V);
         LinkedList<Arista> AristasOrdenadas = new LinkedList<>();
         for (LinkedList<Arista> i : adj) {
             for (Arista j : i) {
@@ -170,7 +173,7 @@ public class Grafo_Listas_Adyacencia {
         return Arbol_Recubridor_Minimo;
     }
     /////// ARBOL RECUBRIDOR MINIMO ///////
-    
+
     /////// CAMINO MAS CORTO ///////
     public void Dijkstra(int origen, int destino) {
         int[] distancia = new int[V];
@@ -190,7 +193,7 @@ public class Grafo_Listas_Adyacencia {
             int u = pq.poll();
             if (!visitado[u]) {
                 visitado[u] = true;
-                for (Arista i : adj[u]) {
+                for (Arista i : adj.get(u)) {
                     if (!visitado[i.d] && distancia[u] + i.peso < distancia[i.d]) {
                         distancia[i.d] = distancia[u] + i.peso;
                         pq.add(i.d);
@@ -201,7 +204,8 @@ public class Grafo_Listas_Adyacencia {
         }
 
         if (distancia[destino] != Integer.MAX_VALUE) {
-            System.out.println("El camino mas corto desde " + origen + " hasta " + destino + " tiene un peso de " + distancia[destino]);
+            System.out.println("El camino mas corto desde " + origen + " hasta " + destino + " tiene un peso de "
+                    + distancia[destino]);
             System.out.print("El camino es: " + origen);
             int j = destino;
             Stack camino = new Stack();
@@ -211,64 +215,64 @@ public class Grafo_Listas_Adyacencia {
                 camino.push(j);
             } while (j != origen);
             camino.pop();
-            while(!camino.isEmpty()){
-                System.out.print(" -> "+camino.pop());
+            while (!camino.isEmpty()) {
+                System.out.print(" -> " + camino.pop());
             }
             System.out.println("");
         } else {
             System.out.println("No existe un camino desde " + origen + " hasta " + destino);
         }
     }
-    
-    public void Floyd_Warshall(){
+
+    public void Floyd_Warshall() {
         int[][] W = new int[V][V];
         int[][] R = new int[V][V];
-        
-        for (int i = 0; i< V; i++) {
+
+        for (int i = 0; i < V; i++) {
             for (int j = 0; j < V; j++) {
-                if(j == i){
+                if (j == i) {
                     W[i][j] = 0;
-                }else{
+                } else {
                     W[i][j] = 999999999;
                 }
                 R[i][j] = -1;
             }
         }
-        for(LinkedList<Arista> i : adj){
-            for(Arista j : i){
+        for (LinkedList<Arista> i : adj) {
+            for (Arista j : i) {
                 W[j.o][j.d] = j.peso;
                 R[j.o][j.d] = j.d;
             }
         }
-        
-        for (int k = 0; k< V; k++) {
+
+        for (int k = 0; k < V; k++) {
             for (int i = 0; i < V; i++) {
                 for (int j = 0; j < V; j++) {
-                    if (W[i][k] + W[k][j] < W[i][j]){
+                    if (W[i][k] + W[k][j] < W[i][j]) {
                         W[i][j] = W[i][k] + W[k][j];
                         R[i][j] = k;
                     }
                 }
             }
         }
-        
+
         System.out.println("\n   Matriz de Pesos");
-        for (int i=0; i<V; ++i) {
-            for (int j=0; j<V; ++j) {
+        for (int i = 0; i < V; ++i) {
+            for (int j = 0; j < V; ++j) {
                 if (W[i][j] == 999999999)
                     System.out.print("Inf   ");
                 else
-                    System.out.print(W[i][j]+"   ");
+                    System.out.print(W[i][j] + "   ");
             }
             System.out.println();
         }
         System.out.println("\n   Matriz de Recorridos");
-        for (int i=0; i<V; ++i) {
-            for (int j=0; j<V; ++j) {
+        for (int i = 0; i < V; ++i) {
+            for (int j = 0; j < V; ++j) {
                 if (R[i][j] == -1)
                     System.out.print("-   ");
                 else
-                    System.out.print(R[i][j]+"   ");
+                    System.out.print(R[i][j] + "   ");
             }
             System.out.println();
         }
